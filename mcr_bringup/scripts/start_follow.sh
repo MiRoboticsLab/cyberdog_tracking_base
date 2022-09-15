b1b1d21db6d188b71406a31b490539cb506509b9
张传发 <zhangchuanfa@xiaomi.com>
2022-09-15 17:07:17 +0800

    modify uwb transformer's name

--- mcr_bringup/scripts/start_follow.sh
+++ mcr_bringup/scripts/start_follow.sh
@@ -1,6 +1,6 @@
 ros2 service call /lifecycle_manager_navigation/manage_nodes nav2_msgs/srv/ManageLifecycleNodes "{command: 0}"
-ros2 service call /lifecycle_manager_simulator/manage_nodes nav2_msgs/srv/ManageLifecycleNodes "{command: 0}"
+ros2 service call /lifecycle_manager_mcr_uwb/manage_nodes nav2_msgs/srv/ManageLifecycleNodes "{command: 0}"
 # ros2 service call /is_target_insight_inused std_srvs/srv/SetBool "{data: true}" # side follow use this.
 ros2 action send_goal /tracking_target mcr_msgs/action/TargetTracking "{relative_pos: 1}" --feedback
 ros2 service call /lifecycle_manager_navigation/manage_nodes nav2_msgs/srv/ManageLifecycleNodes "{command: 1}"
-ros2 service call /lifecycle_manager_simulator/manage_nodes nav2_msgs/srv/ManageLifecycleNodes "{command: 1}"
+ros2 service call /lifecycle_manager_mcr_uwb/manage_nodes nav2_msgs/srv/ManageLifecycleNodes "{command: 1}"
