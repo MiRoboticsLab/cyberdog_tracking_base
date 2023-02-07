ccd5604aae0fb8d73d3135ebb014f24b6eed3b3e
duyongquan <duyongquan1@xiaomi.com>
2023-02-07 20:21:58 +0800

    [Fix]  修改导航配置参数
    	1 修改导航配置参数
    
    Signed-off-by: duyongquan <duyongquan1@xiaomi.com>

--- mcr_bringup/launch/bringup_navigate_to_pose_launch.py
+++ mcr_bringup/launch/bringup_navigate_to_pose_launch.py
@@ -48,8 +48,7 @@ def generate_launch_description():
     # https://github.com/ros/robot_state_publisher/pull/30
     # TODO(orduno) Substitute with `PushNodeRemapping`
     #              https://github.com/ros2/launch_ros/issues/56
-    remappings = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
+    remappings = []
 
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
